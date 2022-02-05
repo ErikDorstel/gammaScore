41b25be47c746deb869ead1a13adb29ddfe08fa5
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2022-02-05 09:23:38 +0100

    improve 1 minute average calculation

--- ray.h
+++ ray.h
@@ -1,36 +1,42 @@
-unsigned long rayTimer; unsigned long deltaTimer; unsigned long minuteTimer;
-volatile int rayCount; int rayCountArray[10]; volatile bool alarmEnable; hw_timer_t * timer0=NULL;
+unsigned long rayTimer; unsigned long deltaTimer; unsigned long sec6Timer; unsigned long minuteTimer;
+volatile int rayCount; int rayCountArray1[10]; int rayCountArray10[10]; volatile bool alarmEnable; hw_timer_t * timer0=NULL;
 
 void IRAM_ATTR rayISR() { rayCount++; if (alarmEnable) { ledcWrite(0,128); } timerRestart(timer0); }
 void IRAM_ATTR timer0ISR() { ledcWrite(0,0); }
 
 struct rayStruct {
-  int duration; bool alarmEnable;
-  int lastEventCpm; int min1Cpm; int min10Cpm;
+  int duration1; int duration10; bool alarmEnable;
+  int lastEventClicks; int min1Clicks; int min10Clicks;
   float lastEventSv; float min1Sv; float min10Sv; }; struct rayStruct ray;
 
 void initRay() {
-  ray.alarmEnable=false; alarmEnable=ray.alarmEnable;
-  rayCount=0; for (int x=0;x<=9;x++) { rayCountArray[x]=0; } ray.duration=1;
+  ray.alarmEnable=false; alarmEnable=ray.alarmEnable; ray.duration1=1; ray.duration10=1; rayCount=0;
+  for (int x=0;x<=9;x++) { rayCountArray1[x]=0; rayCountArray10[x]=0; }
   pinMode(18,INPUT); attachInterrupt(18,rayISR,RISING);
   analogSetPinAttenuation(32,ADC_0db); //int a=analogRead(32); if (a>0) { Serial.println(map(a,0,4095,100,950)); }
   ledcSetup(0,100,8); ledcAttachPin(19,0);
   timer0=timerBegin(0,80,true); timerAttachInterrupt(timer0,&timer0ISR,true);
   timerAlarmWrite(timer0,10000,true); timerAlarmEnable(timer0);
   deltaTimer=millis();
+  sec6Timer=millis()+6000;
   minuteTimer=millis()+60000;
   rayTimer=millis()+1000; }
 
 void rayWorker() {
+  if (millis()>=sec6Timer) { sec6Timer=millis()+6000;
+    for (int x=8;x>=0;x--) { rayCountArray1[x+1]=rayCountArray1[x]; }
+    rayCountArray1[0]=0; if (ray.duration1<10) { ray.duration1++; } }
   if (millis()>=minuteTimer) { minuteTimer=millis()+60000;
-    for (int x=8;x>=0;x--) { rayCountArray[x+1]=rayCountArray[x]; }
-    rayCountArray[0]=0; if (ray.duration<10) { ray.duration++; } }
+    for (int x=8;x>=0;x--) { rayCountArray10[x+1]=rayCountArray10[x]; }
+    rayCountArray10[0]=0; if (ray.duration10<10) { ray.duration10++; } }
   if (millis()>=rayTimer) { rayTimer=millis()+1000; alarmEnable=ray.alarmEnable;
-    if (rayCount>0) { int y=rayCount; rayCount=0; rayCountArray[0]+=y; int delta=millis()-deltaTimer; deltaTimer=millis(); ray.lastEventCpm=y; ray.lastEventSv=y*5000.0/delta; }
-    ray.min1Cpm=rayCountArray[0]; ray.min1Sv=rayCountArray[0]/12.0;
-    int y=0; for (int x=0;x<=9;x++) { y+=rayCountArray[x]; }
-    ray.min10Cpm=y; ray.min10Sv=y/12.0/ray.duration;
+    if (rayCount>0) { int y=rayCount; rayCount=0; rayCountArray1[0]+=y; rayCountArray10[0]+=y;
+      int delta=millis()-deltaTimer; deltaTimer=millis(); ray.lastEventClicks=y; ray.lastEventSv=y*5000.0/delta; }
+    int y=0; for (int x=0;x<=9;x++) { y+=rayCountArray1[x]; }
+    ray.min1Clicks=y; ray.min1Sv=y/1.2/ray.duration1;
+    y=0; for (int x=0;x<=9;x++) { y+=rayCountArray10[x]; }
+    ray.min10Clicks=y; ray.min10Sv=y/12.0/ray.duration10;
     if (debug) {
       Serial.print("last Event: " + String(ray.lastEventSv) + " µSv/h");
-      Serial.print(" - current Minute: " + String(ray.min1Sv) + " µSv/h");
-      Serial.println(" - last " + String(ray.duration) + " Minutes: " + String(ray.min10Sv) + " µSv/h"); } } }
+      Serial.print(" - 1 Minute average: " + String(ray.min1Sv) + " µSv/h");
+      Serial.println(" - " + String(ray.duration10) + " Minutes average: " + String(ray.min10Sv) + " µSv/h"); } } }
